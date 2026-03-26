361959cebbeb8b60be0352f02e2ca71088e549b1
Miranda <jintch.miranda14@gmail.com>
2026-03-26 22:15:27 +0400

    Add files via upload

---
+++ Level49/homework/homework.py
@@ -0,0 +1,16 @@
+def number():
+    print(10+19+37*3)
+
+number()   
+
+
+def greet([1,2,3,4,5,6,7,8,9]):
+    print(1:7)
+
+greet()
+
+
+def num(1,2,*):
+    print(1*2)
+
+num()   
---
+++ Level49/level49.py
@@ -0,0 +1,30 @@
+#amocana1
+
+def my_function():
+    print("hellomira"*11)
+
+my_function()   
+
+
+
+#amocana2
+def funcion(a,b):
+ if a==True:
+    print(b)
+ my_function(False,"goa")   
+
+
+
+#amocana3
+def my_function(a,b):
+    print(a[0:b])
+
+my_function([1,2,3,4,5,6,7,8,9,10,11], 6)
+
+
+
+#amocana4
+def my_function(name):
+    print(name*name*name)
+
+my_function(5)
